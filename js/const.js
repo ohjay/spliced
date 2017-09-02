86cd04c26364e5a63826a60f31b7755b7facea1b
Owen Jow <owenjow@berkeley.edu>
2017-09-01 23:20:49 -0700

    Active animal indicator, basic uploads

--- js/const.js
+++ js/const.js
@@ -5,4 +5,8 @@
 
 ID_IMG_FROM = 'img-from';
 ID_IMG_TO = 'img-to';
+ID_UPLOAD = 'upload';
+ID_UPLOAD_BTN = 'button-upload';
+ID_CONTAINER_FROM = 'container-img-from';
+ID_CONFIRM_CROP_BTN = 'confirm-crop';
 ID_ANIMAL_SELECTION = 'animal-selection';
--- js/main.js
+++ js/main.js
@@ -2,15 +2,82 @@
  * Code specific to the page's interface.
  */
 
-$(window).on('load', function() {
-  // Animal selection
+var activeAnimal = null;
+
+function setupImageUploads() {
+  document.getElementById(ID_UPLOAD).addEventListener('change', function() {
+    var img = document.getElementById(ID_IMG_FROM);
+    var file = document.getElementById(ID_UPLOAD).files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function() {
+      // hideCanvasesAndMarkers(); // TODO ?
+      img.style.display = 'none';
+      img.src = reader.result;
+
+      var otherImg = document.getElementById(ID_IMG_TO);
+      var container = document.getElementById(ID_CONTAINER_FROM);
+      container.style.width  = otherImg.clientWidth  + 'px';
+      container.style.height = otherImg.clientHeight + 'px';
+      cropper = new Cropper(img, {
+        cropBoxResizable: false,
+        aspectRatio: otherImg.clientWidth / otherImg.clientHeight,
+        ready: function() {
+          this.cropper.setCropBoxData({
+            left: 0,
+            top: 0,
+            width: otherImg.clientWidth,
+            height: otherImg.clientHeight
+          });
+          img.style.display = 'inline';
+        }
+      });
+
+      $('#' + ID_UPLOAD_BTN).css('display', 'none');
+      $('#' + ID_CONFIRM_CROP_BTN).css('display', 'inline-block');
+    }
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  }, true);
+  
+  $('#' + ID_CONFIRM_CROP_BTN).click(function(evt) {
+    var otherImg = document.getElementById(ID_IMG_TO);
+    var croppedCvs = cropper.getCroppedCanvas({
+      width: otherImg.clientWidth,
+      height: otherImg.clientHeight
+    });
+    cropper.destroy();
+    var img = document.getElementById(ID_IMG_FROM);
+    img.src = croppedCvs.toDataURL();
+    // showCanvasesAndMarkers(); // TODO ?
+    $('#' + ID_CONFIRM_CROP_BTN).css('display', 'none');
+    $('#' + ID_UPLOAD_BTN).css('display', 'inline-block');
+  });
+}
+
+function setupAnimalSelection() {
   var container = document.getElementById(ID_ANIMAL_SELECTION);
   var images = container.getElementsByTagName('img');
   var i, src;
   for (i = 0; i < images.length; ++i) {
+    if (images[i].classList.contains('animal-active')) {
+      activeAnimal = images[i];
+    }
     images[i].onclick = function() {
+      if (activeAnimal != null) {
+        $(activeAnimal).removeClass('animal-active');
+      }
       src = this.src;
       $('#' + ID_IMG_TO).attr('src', src.replace('_small', ''));
+      activeAnimal = this;
+      $(activeAnimal).addClass('animal-active');
     }
   }
+}
+
+$(window).on('load', function() {
+  setupImageUploads();
+  setupAnimalSelection();
 });
