e609da36cab019872a4c5d99ab0af0dc335ef115
Owen Jow <owenjow@berkeley.edu>
2017-09-02 02:17:53 -0700

    Working on confirm button behavior

--- js/const.js
+++ js/const.js
@@ -3,12 +3,17 @@
  * and used by `facemorphing.js` and `main.js`.
  */
 
-ID_IMG_FROM = 'img-from';
-ID_IMG_TO = 'img-to';
-ID_UPLOAD = 'upload';
-ID_UPLOAD_BTN = 'button-upload';
-ID_CONTAINER_FROM = 'container-img-from';
-ID_CONFIRM_CROP_BTN = 'confirm-crop';
-ID_CONFIRM_IMG_BTN = 'confirm-image';
-ID_TAKE_PICTURE_BTN = 'take-picture';
-ID_ANIMAL_SELECTION = 'animal-selection';
+const ID_IMG_FROM = 'img-from';
+const ID_IMG_TO = 'img-to';
+const ID_UPLOAD = 'upload';
+const ID_UPLOAD_BTN = 'button-upload';
+const ID_CONTAINER_FROM = 'container-img-from';
+const ID_CONFIRM_CROP_BTN = 'confirm-crop';
+const ID_CONFIRM_IMG_BTN = 'confirm-image';
+const ID_TAKE_PICTURE_BTN = 'take-picture';
+const ID_ANIMAL_SELECTION = 'animal-selection';
+
+const DEFAULT_POINTS_FILEPATH = 'data/default.min.json';
+const MARKER_DIR = 'images/markers/';
+const MARKER_CYCLE  = ['gold.png', 'blue.png', 'green.png',
+                       'red.png', 'purple.png', 'white.png'];
--- js/main.js
+++ js/main.js
@@ -3,6 +3,41 @@
  */
 
 var activeAnimal = null;
+var currMarkerId = 0;
+var inv          = {}; // marker ID # --> index in respective `points` array
+
+function findPosition(elt) {
+  if (typeof(elt.offsetParent) != 'undefined') {
+    for (var posX = 0, posY = 0; elt; elt = elt.offsetParent) {
+      posX += elt.offsetLeft;
+      posY += elt.offsetTop;
+    }
+    return [posX, posY];
+  }
+  return [elt.x, elt.y];
+}
+
+/*
+ * Draw markers for an already-existent array of points.
+ */
+function drawMarkers(id, imgPos) {
+  var relevantPoints = points[id];
+  var numPoints = relevantPoints.length;
+  var pt, i, markerSrc;
+  
+  var mi = 0;
+  for (i = 0; i < numPoints; ++i) {
+    markerSrc = MARKER_DIR + MARKER_CYCLE[mi];
+    mi = (mi + 1) % MARKER_CYCLE.length;
+    
+    pt = relevantPoints[i];
+    document.body.appendChild(createMarker('marker' + currMarkerId, markerSrc));
+    $('#marker' + currMarkerId).css('left', pt[0] + imgPos[0] - 5)
+                               .css('top',  pt[1] + imgPos[1] - 5).show();
+    inv[currMarkerId] = i;
+    ++currMarkerId;
+  }
+}
 
 function setupImageUploads() {
   document.getElementById(ID_UPLOAD).addEventListener('change', function() {
@@ -82,7 +117,89 @@ function setupAnimalSelection() {
   }
 }
 
+function setupImageConfirm() {
+  $('#' + ID_CONFIRM_IMG_BTN).click(function(evt) {
+    $.getJSON(DEFAULT_POINTS_FILEPATH, function(data) {
+      var positions = data.points.map(elt => [elt[0] * width, elt[1] * height]);
+      points = {ID_IMG_FROM: [], ID_IMG_TO: []};
+      for (var i = 0; i < positions.length; ++i) {
+        points[ID_IMG_FROM].push(positions[i]);
+      }
+      console.log(positions.length); // TODO remove
+      console.log('---'); // TODO remove
+      
+      // Left side points
+      points[ID_IMG_FROM].push([0.75 * positions[0][0], positions[0][1]]);
+      points[ID_IMG_FROM].push([0.75 * positions[0][0], positions[2][1]]);
+      // Right side points
+      var img = document.getElementById(ID_IMG_FROM);
+      var rdx = 0.25 * (img.clientWidth - positions[14][0]);
+      points[ID_IMG_FROM].push([rdx + positions[14][0], positions[14][1]]);
+      points[ID_IMG_FROM].push([rdx + positions[14][0], positions[12][1]]);
+      // Top points
+      points[ID_IMG_FROM].push([positions[19][0], 0.30 * positions[19][1]]);
+      points[ID_IMG_FROM].push([positions[22][0], 0.25 * positions[22][1]]);
+      points[ID_IMG_FROM].push([positions[18][0], 0.25 * positions[18][1]]);
+      points[ID_IMG_FROM].push([positions[15][0], 0.30 * positions[15][1]]);
+
+      // Draw the markers
+      drawMarkers(ID_IMG_FROM, findPosition(img));
+      setupMarkers();
+    });
+  });
+}
+
+function setupMarkers() {
+  function launchMarkerAdjustment(evt) {
+    if (!evt) {
+      var evt = window.event;
+    }
+    var target = evt.target || evt.srcElement;
+    if (!target.id.startsWith('marker')) {
+      return;
+    }
+
+    var canvasId = ID_CVS_FROM;
+    relevCtx = document.getElementById(canvasId).getContext('2d');
+
+    var relevImg = document.getElementById(ID_IMG_FROM);
+    relevWidth  = relevImg.clientWidth;
+    relevHeight = relevImg.clientHeight;
+    relevMarkerNo = parseInt(target.id.match(/\d+$/)[0], 10);
+    relevPos = findPosition(relevImg);
+    document.addEventListener('mousemove', doMarkerAdjustment);
+    return false;
+  }
+
+  function doMarkerAdjustment(evt) {
+    if (!evt) {
+      var evt = window.event;
+    }
+    var target = evt.target || evt.srcElement;
+    var inImgCoords = [
+      evt.pageX - relevPos[0],
+      evt.pageY - relevPos[1]
+    ];
+
+    if (inImgCoords[0] >= 0 && inImgCoords[0] < relevWidth &&
+        inImgCoords[1] >= 0 && inImgCoords[1] < relevHeight) {
+      $('#marker' + relevMarkerNo).css('left', evt.pageX - 5).css('top', evt.pageY - 5);
+      points[ID_IMG_FROM][inv[relevMarkerNo]] = inImgCoords;
+      relevCtx.clearRect(0, 0, relevWidth, relevHeight);
+    }
+    return false;
+  }
+
+  function finishMarkerAdjustment(evt) {
+    document.removeEventListener('mousemove', doMarkerAdjustment);
+  }
+
+  document.onmousedown = launchMarkerAdjustment;
+  document.onmouseup   = finishMarkerAdjustment;
+}
+
 $(window).on('load', function() {
   setupImageUploads();
   setupAnimalSelection();
+  setupImageConfirm();
 });
